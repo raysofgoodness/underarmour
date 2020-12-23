b8fe62c30d2f8cca151917d5394241956619babd
Skoropad <shady5324@gmail.com>
2020-12-23 22:14:07 +0200

    added favorites-button modificator

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,11 @@
+
 $(document).ready(function(){
   $('.burger-menu').click(function(event){
     $('.burger-menu').toggleClass('menu-open');
+  });
+
+  $('.product-item__favorite').click(function(event){
+    $(this).toggleClass('product-item__favorite--active');
   });
 
 });
